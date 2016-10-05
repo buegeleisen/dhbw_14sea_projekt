2cd0f67d42d6a206deb78f312b58db46732590b9
mrponcho <email@johannesbarth.com>
2016-10-05 11:58:39 +0200

    Statemachine

---
+++ Statemachine/src/main/java/statemachine/State.java
@@ -0,0 +1,18 @@
+package statemachine;
+
+/**
+ * Created by mrpon on 05.10.2016.
+ */
+public class State {
+    public static State L1 = new State();
+    public static State L2 = new State();
+    public static State L3 = new State();
+    public static State L4 = new State();
+    public static State L5 = new State();
+    public static State Drilling = new State();
+    public static State Milling = new State();
+
+
+
+
+}
