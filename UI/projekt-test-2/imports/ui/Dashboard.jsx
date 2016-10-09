0e20d375706fe96991ac0407eff225ca64d1d482
mrponcho <email@johannesbarth.com>
2016-10-09 18:06:58 +0200

    projekt test 2
    Bisherige UI Impl.

---
+++ UI/projekt-test-2/imports/ui/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar.jsx';
+import Index from './Index.jsx';
+
+const dashboard = (
+<div className="">
+  <Navbar/>
+  <Index/>
+</div>
+
+);
+
+export default React.createClass({
+
+
+  render() {
+      return (
+          dashboard
+            );
+        }
+  });
