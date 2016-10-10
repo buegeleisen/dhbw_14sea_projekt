48fc08dee932b054c5d52ead631ba9075e9c254f
mrponcho <email@johannesbarth.com>
2016-10-10 20:25:14 +0200

    Machines Layout
    Machines Layout mit 4 Panels aufgebaut.
    Unnötige Daten entfernt

---
+++ UI/projekt-test-2/imports/ui/JaCom.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+const logo = (
+  <div><h1 className="ja">JA</h1><h1 className="com">.COM</h1></div>
+);
+
+export default React.createClass({
+
+
+  render() {
+      return (
+          logo
+            );
+        }
+});
--- UI/projekt-test-2/imports/ui/Machines.jsx
+++ UI/projekt-test-2/imports/ui/Machines.jsx
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-
+import TemperatureChart from './TemperatureChart.jsx';
 import {Panel} from 'react-bootstrap';
 
 const title = (
-<h3>Temperature</h3>
+<h3>Temperature Chart</h3>
 );
+
 
 const machines = (
 
-
-const panelsInstance = (
-  <div>
-    <Panel header={title}>
-      Panel content
-    </Panel>
+  <div className="col-md-12 container">
+    <div >
+      <h1 className="section-heading">Machine Overview</h1>
+    </div>
+    <div className="col-md-6">
+      <Panel header={title}>
+        <TemperatureChart />
+      </Panel>
+    </div>
+    <div className="col-md-6">
+      <Panel header="Drilling Speed">
+        <TemperatureChart />
+      </Panel>
+    </div>
+    <div className="col-md-6">
+      <Panel header="Milling Speed">
+        <TemperatureChart />
+      </Panel>
+    </div>
+    <div className="col-md-6">
+      <Panel header="Other Stuff">
+        <TemperatureChart />
+      </Panel>
+    </div>
   </div>
 );
-);
+
 
 export default React.createClass({
 
