0589c545e8fff0d52f676e1cc966dfc1a7e13b94
mrponcho <email@johannesbarth.com>
2016-10-14 15:06:57 +0200

    UI Changes
    Component für Logo (Clickable mit href) in Navbar
    Charts mit Variable in der die DB Arrays übergeben werden

--- UI/projekt-test-2/imports/ui/JaCom.jsx
+++ UI/projekt-test-2/imports/ui/JaCom.jsx
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 
-const logo = (
-  <div><h1 className="ja">JA</h1><h1 className="com">.COM</h1></div>
-);
-
-export default React.createClass({
-
+export default class JaCom extends Component{
 
   render() {
+    const logo = (
+      <div className="logo"><a href={this.props.route}><p className="ja">JA</p><p className="com">.COM</p></a></div>
+    );
+
       return (
           logo
             );
         }
-});
+};
+
+JaCom.propTypes = {
+  route: PropTypes.string
+};
