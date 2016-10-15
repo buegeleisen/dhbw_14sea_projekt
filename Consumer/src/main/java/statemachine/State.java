e3284c70cad46da17d649a16a6ec0217bf48c2d3
Uno1404 <goel.onur@gmx.de>
2016-10-15 15:37:57 +0200

    Activemqconsumer in Testphase & Poms ausgeführt
    Lel Test was geht ?

---
+++ Consumer/src/main/java/statemachine/State.java
@@ -0,0 +1,21 @@
+package statemachine;
+
+/**
+ * Created by mrpon on 05.10.2016.
+ */
+public class State {
+    public static State q0 = new State();
+    public static State q1 = new State();
+    public static State q2 = new State();
+    public static State q3 = new State();
+    public static State q4 = new State();
+    public static State q5 = new State();
+    public static State q6 = new State();
+    public static State q7 = new State();
+    public static State q8 = new State();
+    public static State q9 = new State();
+
+
+
+
+}
