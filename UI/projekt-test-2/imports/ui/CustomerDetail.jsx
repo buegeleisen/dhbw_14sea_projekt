c0ac4325b7e186a76ca641eed3285b857e7588b3
Miguel Moreno <miguel.moreno@gmx.de>
2016-11-08 17:59:12 +0100

    ui

--- UI/projekt-test-2/imports/ui/CustomerDetail.jsx
+++ UI/projekt-test-2/imports/ui/CustomerDetail.jsx
@@ -1,3 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import {Panel} from 'react-bootstrap';
+import CustomerData from '../api/customerData.js';
+
+export default class CustomerDetail extends Component {
+  
+}
