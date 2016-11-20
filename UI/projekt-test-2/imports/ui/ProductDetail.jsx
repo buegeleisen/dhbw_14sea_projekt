e3503282239c6dc060dce375ed3ef386a6448f3d
mrponcho <email@johannesbarth.com>
2016-11-20 23:05:16 +0100

    UI
    LineCharts aus Product Detail genommen.
    JS Error

--- UI/projekt-test-2/imports/ui/ProductDetail.jsx
+++ UI/projekt-test-2/imports/ui/ProductDetail.jsx
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import BarChart from './BarChart.jsx';
 import {Panel, ListGroup, ListGroupItem} from 'react-bootstrap';
-import {LineChart} from 'react-easy-chart';
 import * as Analysis from '../api/analysis.js';
 
 export default class ProductDetail extends Component {
@@ -27,16 +26,11 @@ export default class ProductDetail extends Component {
     return files;
   }
 
-  getLineArray(array){
-    let total = [];
-    for(let i = 0; i<array.length; i++){
-        let data = {"x": i,"y": array[i]};
-        total.push(data);
-    }
-  }
+
 
   render() {
     console.log(this.props.product);
+    console.log(this.props.product.machineData.millingHeat);
 
     const render =(
       <div className="col-md-12 container">
@@ -119,55 +113,6 @@ export default class ProductDetail extends Component {
           </Panel>
           </div>
 
-          <div className="col-md-12">
-            <h1 className="section-heading">Milling & Drilling Overview</h1>
-            <div className="col-md-6">
-              <Panel header="Milling Temperature">
-                <LineChart
-                  axes
-                  interpolate={'cardinal'}
-                  data={[this.props.product.machineData.millingHeat]}
-                  width={400}
-                  height={300}
-                  />
-              </Panel>
-            </div>
-            <div className="col-md-6">
-              <Panel header="Milling Speed">
-                <LineChart
-                  axes
-                  interpolate={'cardinal'}
-                  data={[this.props.product.machineData.millingSpeed]}
-                  width={400}
-                  height={300}
-                  />
-              </Panel>
-            </div>
-          <div className="col-md-6">
-            <Panel header="Drilling Temperature">
-              <LineChart
-                axes
-                interpolate={'cardinal'}
-                data={[this.props.product.machineData.drillingHeat]}
-                width={400}
-                height={300}
-                />
-            </Panel>
-          </div>
-          <div className="col-md-6">
-            <Panel header="Drilling Speed">
-              <LineChart
-                axes
-                interpolate={'cardinal'}
-                data={[this.props.product.machineData.drillingSpeed]}
-                width={400}
-                height={300}
-                />
-            </Panel>
-          </div>
-        </div>
-      </div>
-    );
 
       return (
           render
